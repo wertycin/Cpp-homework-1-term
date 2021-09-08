018a1c9e0bdf1cbe7475a6e98a4350de13f56432
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-09-08 21:27:31 +0300

    Add files via upload

--- Homework3.cpp
+++ Homework3.cpp
@@ -1,60 +1,25 @@
 #include <iostream>
 #include <iomanip>
-//#include <cmath>
+#include <cmath>
 #include <limits>
 
 int main()
 {
-	double a, b, c, D, x1, x2;
-	const double epsilon =
-		std::numeric_limits < double > ::epsilon();
+	double charge, tenseness, potential;
 
+	const int charge_conversion = 3 * pow(10, 9);
 
-	std::cout << "Enter coefficients 'a', 'b', 'c' with space: ";
-	
-	std::cin >> a >> b >> c;
+	const double tenseness_conversion = 3.3 * pow(10, -5);
 
-	if (abs(a) > epsilon)
-	{
+	const double potential_conversion = 1.0 / 300;
 
-		D = (pow(b, 2) - 4 * a * c);
+	std::cout <<
+		"Please enter your values in Coulumbs, Volts per meter and Volts in SI system: ";
 
-		if (D > epsilon)
-		{
-			x1 = (-b + pow(D, 0.5)) / (2 * a);
-			x2 = (-b - pow(D, 0.5)) / (2 * a);
-			std::cout << "Your solutions are: " << 
-				std::setprecision(3) << std::fixed << x1 << " and " << x2 << '.';
-		}
-		else if (abs(D) < epsilon)
-		{
-			x1 = (-b) / (2 * a);
-			std::cout << "Your solution is: " << 
-				std::setprecision(3) << std::fixed << x1;
-		}
-		else if (D < -epsilon)
-		{
-			std::cout << "Solution doesn't exist.";
-		}
-	}
-	else if (abs(b) > epsilon)
-	{
-		x1 = -c / b;
-		std::cout << "Your solution is: " <<
-			std::setprecision(3) << std::fixed << x1;
+	std::cin >> charge >> tenseness >> potential;
 
-	}
-	
-	else if (abs(c) > epsilon)
-	{
-		std::cout << "Solution doesn't exist.";
-	}
-
-	else
-	{
-		std::cout << "Any x suits as a solution.";
-	}
-	
-	return 0;
+	std::cout << "Your answer in SGS system: " << charge * charge_conversion <<
+		" F, " << tenseness * tenseness_conversion << " E, " <<
+		potential * potential_conversion << " units of potential CGSE.";
 
 }
