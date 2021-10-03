311af4454d1d0164ccb1c1db821c63a99894704b
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-10-03 16:02:54 +0300

    Update Header.hpp

--- HW5/Header.hpp
+++ HW5/Header.hpp
@@ -16,7 +16,7 @@ public:
 	void basketball_scores_up();
 	void add_teammates(const std::string& mate);
 	void add_teammates(const std::vector <std::string>& friends);
-	void print();
+	void print() const;
 
 private:
 	int level;
@@ -24,7 +24,32 @@ private:
 	std::vector <std::string> team;
 };
 
+
+class Energy
+{
+public:
+	Energy(double value, std::string flag) : value(value), flag(flag)
+	{}
+	Energy()
+	{}
+
+	const std::vector <double> convertation()
+	{
+		flag == "j" ? value_j = value, value_erg = value * pow(10, 7),
+			value_ev = (value * pow(10, 19))/(1,6) : flag == "erg" ? value_j = value * pow(10, -7),
+			value_erg = value, value_ev = (value * pow(10, 12))/(1,6) : value_j = value * (1,6) * pow(10, -19),
+			value_erg = value * (1,6) * pow(10, -12), value_ev = value;
+
+		return { value_j, value_erg, value_ev };
+	}
+
+	double value, value_j, value_erg, value_ev;
+	std::string flag;
+};
+
+
+
 int num1();
-//int num2();
+int num2();
 //int num3();
 //int num4();
